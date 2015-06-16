b5ed0f1e7f7312358587a1a96109deeaf99b93d1
Andrew Pierno <andrewpierno@gmail.com>
2015-06-16 05:45:23 -0700

    working on setting dom elements inside a tag

--- public/js/ap.js
+++ public/js/ap.js
@@ -23,25 +23,12 @@ $(document).ready(function() {
 
 
 // click handler for button
-var buttonLoader = function(e) {
+function buttonLoader(e) {
+  //console.log('clicked');
   e.preventDefault();
   e.stopPropagation();
   e.target.classList.add('loading');
   e.target.setAttribute('disabled','disabled');
-
-  $('#gridSectionH2').hide(300, function() {
-
-  });
-    $('#gridSection').hide(300, function() {});
-
-      $('#formSection').show(300);
-
-      e.target.classList.remove('loading');
-      e.target.removeAttribute('disabled');
-
-      $('button').css('background-color', 'transparent');
-
-      window.eventDataIndex = $(e.target)[0].closest('li').id || null;
 
       $('button').css('background-color', 'transparent');
       var events = JSON.parse(localStorage.getItem('events'));
@@ -52,31 +39,45 @@ var buttonLoader = function(e) {
           + event.location_name
           + ")");
 
-  // setTimeout(function(){
+  setTimeout(function(){
+    $('#gridSectionH2').hide(300, function() {
+      $('#gridSection').hide(300, function() {
+        $('#formSection').show(300);
+      }); // end hide grid section
+    }); // end hide grid section h2
 
-  //
-  //   // index of selected element from data array.
-  //   window.eventDataIndex = $(e.target)[0].closest('li').id || null;
-  //
-  //
-  //         $('button').css('background-color', 'transparent');
-  //         var events = JSON.parse(localStorage.getItem('events'));
-  //         var event = events[window.eventDataIndex];
-  //         $('#event-info').text("You selected: "
-  //             + event.event_title
-  //             + " ("
-  //             + event.location_name
-  //             + ")");
-  //
-  //
-  //
-  //     },1500);
+
+
+
+        e.target.classList.remove('loading');
+        e.target.removeAttribute('disabled');
+
+        $('button').css('background-color', 'transparent');
+
+        window.eventDataIndex = $(e.target)[0].closest('li').id || null;
+
+    // index of selected element from data array.
+    window.eventDataIndex = $(e.target)[0].closest('li').id || null;
+
+
+          $('button').css('background-color', 'transparent');
+          var events = JSON.parse(localStorage.getItem('events'));
+          var event = events[window.eventDataIndex];
+          $('#event-info').text("You selected: "
+              + event.event_title
+              + " ("
+              + event.location_name
+              + ")");
+
+
+
+      },1500);
     }; // end of click event function
 
 
 // create nodes for each event.
 function setEvents(data) {
-
+  console.log(data.length);
   for (var i=0; i<data.length; i++) {
     if (!data[i].color_pallet.c1) {
       data[i].color_pallet.c1 = 'rgb(247, 124, 51)';
@@ -85,35 +86,42 @@ function setEvents(data) {
     //create list element, append to grid container
     $( "<li id='" + i + "'>" ).appendTo($('.cbp-ig-grid'));
     // create element, append inside list element
-    $("<a href='#' style='color:'" + data[i].color_pallet.c1 +"'>").appendTo($('#' + i));
+    $("<a href='#doNothing' id='#a'"+i+" style='color:'" + data[i].color_pallet.c1 +"'>").appendTo($('#' + i));
+    $('li').on('click', '#doNothing', function(e) {
+      e.preventDefault();
+      return false;
+    })
 
     if (data[i].away_team_logo) {
-      $("<img src=https://cdn.pogoseat.com" + data[i].away_team_logo + ">").appendTo($('#' + i));
+      $("<img src=https://cdn.pogoseat.com" + data[i].away_team_logo + ">").appendTo($('#a' + i));
     } else {
-      $("<img src=https://cdn.pogoseat.com" + data[i].home_team_logo + ">").appendTo($('#' + i));
+      $("<img src=https://cdn.pogoseat.com" + data[i].home_team_logo + ">").appendTo($('#a' + i));
     }
 
-    $("<h3 class='cbp-ig-title changeColor'>" + data[i].event_title + "</h3>").appendTo($('#' + i));
-    $("<p>" + data[i].timeTillLive + " @ " + data[i].location_name+ "</p>").appendTo($('#' + i));
-    $("<span class='cbp-ig-category'> </span>").appendTo($('#' + i));
+    $("<h3 class='cbp-ig-title changeColor'>" + data[i].event_title + "</h3>").appendTo($('#a' + i));
+    $("<p>" + data[i].timeTillLive + " @ " + data[i].location_name+ "</p>").appendTo($('#a' + i));
+    $("<span class='cbp-ig-category'> </span>").appendTo($('#a' + i));
 
-    $("<button onclick="+buttonLoader+"label='Select' style='color:white;background-color:" + data[i].color_pallet.c1 +"'> Select </button>").appendTo($('#' + i));
+    $("<button label='Select' style='color:white;background-color:" + data[i].color_pallet.c1 +"'> Select </button>").appendTo($('#a' + i));
+    $('a').on('click', 'button', buttonLoader);
 
     $('.cbp-ig-title:before').css('background', data[i].color_pallet.c1);
 
+    $("a").hover(function(){
+      $('h3').css("color", data[0].color_pallet.c1);
+      $('p').css("color", data[0].color_pallet.c1);
+    })
+
   } // end loop
 
-  $("a").click(function(event){
-    event.preventDefault();
-    $('div').removeClass('checkmark');
-    // index from our data array!
-    var dataElement = $(this).parent().attr('id');
-  });
+  // $("a").click(function(event){
+  //   event.preventDefault();
+  //   $('div').removeClass('checkmark');
+  //   // index from our data array!
+  //   var dataElement = $(this).parent().attr('id');
+  // });
 
-  $("a").hover(function(){
-    $('h3').css("color", data[0].color_pallet.c1);
-    $('p').css("color", data[0].color_pallet.c1);
-  })
+
 }
 
 
